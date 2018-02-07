246f92054aa8f26d9432b052e09e9a2cc1618575
PooshS <poosh@meta.ua>
2018-02-07 22:43:25 +0200

    serv port was changed

--- src/main/java/ua/poosh/todo/config/ConfigHolder.java
+++ src/main/java/ua/poosh/todo/config/ConfigHolder.java
@@ -11,6 +11,10 @@ public class ConfigHolder {
     public ConfigHolder(String path) throws IOException {
         properties = new Properties();
         properties.load(new FileInputStream(path));
+        String SERVER_PORT = System.getenv("PORT");
+        if(SERVER_PORT != null){
+            properties.setProperty("app.port",SERVER_PORT);
+        }
     }
 
     public String getProperty(String key){
